e134cf535415128d7a7ed509183f739cd2d5ee3d
xuya <15930167221@163.com>
2017-01-17 17:22:52 +0800

    yxy

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -9,6 +9,13 @@ class IndexController extends Controller {
         $this->display();
     }
     public function dengji(){
+    	// echo "string";
+    	$Data     = M('STATION_P');// 实例化Data数据模型
+        $result     = $Data->select();
+        dump($result);die;
+        $this->assign('result',$result);
         $this->display();
+    
+        // $this->display();
     }
 }
