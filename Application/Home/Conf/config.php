fd4047c53b99e672bbbef312e60ae1440223cdc4
xuya <15930167221@163.com>
2017-02-06 08:33:46 +0800

    yxy

--- Application/Home/Conf/config.php
+++ Application/Home/Conf/config.php
@@ -3,10 +3,10 @@ return array(
 	//'配置项'=>'配置值'
 	// 'DB_TYPE'   => 'sqlsrv', // 数据库类型
 	'DB_TYPE'   => 'mysql', // 数据库类型
-	'DB_HOST'   => 'localhost', // 服务器地址
-	'DB_NAME'   => 'aa', // 数据库名
-	'DB_USER'   => 'root', // 用户名
-	'DB_PWD'    => '', // 密码
+	'DB_HOST'   => '192.168.2.249', // 服务器地址
+	'DB_NAME'   => 'pykj', // 数据库名
+	'DB_USER'   => 'myuser', // 用户名
+	'DB_PWD'    => 'mypassword', // 密码
 	// 'DB_PORT'   => 3306, // 端口
 	// 'DB_PREFIX' => 'think_', // 数据库表前缀 
 	'DB_CHARSET'=> 'utf8', // 字符集
--- Application/Runtime/Cache/Home/e79ccf53c51c2c1282dfd5d080ba3cd8.php
+++ Application/Runtime/Cache/Home/e79ccf53c51c2c1282dfd5d080ba3cd8.php
@@ -27,63 +27,69 @@
                 </div>
             </div>
         </div>
-        <form action="#" method="post">
+        <form action="<?php echo U('Index/jiankang');?>" method="post">
             <div class="inf center">
                 <table border="0" class="mbt center">
                     <tr>
                         <td width="15%">病历号：</td>
-                        <td width="35%"><input type="text" readonly="readonly" id="blNum"></td>
+                        <td width="35%"><input type="text" value="<?php echo (session('id')); ?>" name="br_id" readonly="readonly" id="blNum"></td>
                         <td width="15%"></td>
                         <td width="35%"></td>
                     </tr>
                     <tr>
                         <td>姓名：</td>
-                        <td><input type="text" id="userName1"></td>
+                        <td><input type="text" name="br_name" id="userName1"></td>
                         <td>性别：</td>
                         <td>
-                            <label><input type="radio" name="sex1" checked="checked"><span>男</span></label>
-                            <label><input type="radio" name="sex1"><span>女</span></label>
+                            <label><input type="radio" name="xb" value="男" checked="checked"><span>男</span></label>
+                            <label><input type="radio" name="xb" value="女"><span>女</span></label>
                         </td>
                     </tr>
                     <tr>
                         <td>年龄：</td>
-                        <td><input type="text" id="age1"></td>
+                        <td><input type="text" name="nl" id="age1"></td>
                         <td>出生年月：</td>
-                        <td><input type="text" placeholder="请选择时间！" id="datebut" onClick="jeDate({dateCell:'#datebut',isTime:true,format:'YYYY-MM-DD'})" readonly="readonly"></td>
+                        <td><input type="text" name="cs_date" placeholder="请选择时间！" id="datebut" onClick="jeDate({dateCell:'#datebut',isTime:true,format:'YYYY-MM-DD'})" readonly="readonly"></td>
                     </tr>
                     <tr>
                         <td>身份证号：</td>
-                        <td><input type="text" id="idNum1"></td>
+                        <td><input type="text" name="pass" id="idNum1"></td>
                         <td>电话：</td>
-                        <td><input type="text" id="phone1"></td>
+                        <td><input type="text" name="tel" id="phone1"></td>
                     </tr>
                     <tr>
                         <td>传真：</td>
-                        <td><input type="text" id="chuanzhen1"></td>
+                        <td><input type="text" name="fax" id="chuanzhen1"></td>
                         <td>E-Mail：</td>
-                        <td><input type="text" id="mail1"></td>
+                        <td><input type="text" name="e_mail" id="mail1"></td>
                     </tr>
                     <tr>
                         <td>挂号费：</td>
-                        <td><input type="text"></td>
+                        <td><input type="text" name="ghf"></td>
                         <td width="15%">预约日期：</td>
-                        <td width="35%"><input type="text" class="yyrq" readonly="readonly"></td>
+                        <td width="35%"><input type="text" name="P_date" class="yyrq" readonly="readonly"></td>
                     </tr>
                     <tr>
                         <td>单位：</td>
-                        <td colspan="3"><textarea name="comp" id="comp1"class="lontext"></textarea></td>
+                        <td colspan="3"><textarea name="dw" id="comp1"class="lontext"></textarea></td>
                     </tr>
+                    <!-- 就诊时间 -->
+                    <input type="hidden"  name="jz_date" class="yyrq">
                 </table>
             </div>
-        </form>
+
         <div class="but">
             <div class="butt">
-                <a href=""><button type="button" class="btn btn-warning first">保存</button></a>
+                <!-- 通过js使form地址改变 -->
+                <input type="button" id="jsPreservation" class="btn btn-warning first" value="保存">
+                <!-- <button type="button" class="btn btn-warning first">保存</button> -->
             </div>
             <div class="butt">
-                <a href=""><button type="button" class="btn btn-warning last">就诊</button></a>
+                <input type="submit" class="btn btn-warning last" value="就诊" >
+                <!-- <button type="button" class="btn btn-warning last">就诊</button> -->
             </div>
         </div>
+        </form>
     </div>
 </div>
 <script type="text/javascript">
@@ -91,4 +97,12 @@
 </script>
 </body>
 </html>
+<script type="text/javascript">
+    $("#jsPreservation").click(function(){
+        $("form").attr("action", "<?php echo U('Index/hzbc');?>" ).submit();
+
+    });
+    // var jsPreservation=document.getElementById('jsPreservation');
+
+</script>
 <script src="/zySystem/Public/js/shijian.js"></script>
